1f6e96806c71242604dc59771a104ae476d06034
Khem Pant <khem0026@edu.easj.dk>
2019-10-20 00:42:16 +0200

    PropertiesLoaded

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        private string title;
+        private string author;
+        private int pageNumber;
+        private string isbn13;
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageNumber { get; set; }
+        public string Isbn13 { get; set; }
+        public Book(string title, string author, int pageNumber, string isbn13)
+        {
+            Title = title;
+            Author = author;
+            PageNumber = pageNumber;
+            Isbn13 = isbn13;
+        }
+        public override string ToString()
+        {
+            return $"{Title}-{Author}-{PageNumber}-{Isbn13}";
+        }
+    }
+}
